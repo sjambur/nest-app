88d058d908bf9e44f0f2a66aec5a994daa2474be
Sachin Jambur <sachinjambur@Sachins-MBP.cg.shawcable.net>
2021-10-19 17:43:31 -0600

    Initial commit

---
+++ src/common/middleware/validStudent.middleware.ts
@@ -0,0 +1,20 @@
+import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { students } from '../../db';
+
+@Injectable()
+export class ValidStudentMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const studentId = req.params.studentId;
+
+    const studentExists = students.some((student) => {
+      return student.id === studentId;
+    });
+
+    if (!studentExists) {
+      throw new HttpException('Student not found', 400);
+    }
+
+    next();
+  }
+}
